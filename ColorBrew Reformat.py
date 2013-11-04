e450787590d9c2dd59c26a9edcfca6c0818fabd3
Colin Gerber <colin.gerber@gmail.com>
2013-11-04 11:58:29 -0800

    Pandas code used to reformat colorbrew excel file

---
+++ ColorBrew Reformat.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+import pandas as pd
+from pandas.io.excel import ExcelFile
+
+# <codecell>
+
+infile = ExcelFile('ColorBrewer_all_schemes_RGBonly3.XLS')
+
+# <codecell>
+
+df = infile.parse(infile.sheet_names[0])
+
+# <codecell>
+
+df_fill = df.fillna(method = 'ffill')
+
+# <codecell>
+
+df_fill.head()
+
+# <codecell>
+
+df.head()
+
+# <codecell>
+
+#df_key = pd.factorize(pd.lib.fast_zip([df_fill.ColorName, df_fill.NumOfColors]))
+
+# <codecell>
+
+#df_key[0]
+
+# <codecell>
+
+#df_fill.index = df_key[0]
+
+# <codecell>
+
+df_fill.head(40)
+
+# <codecell>
+
+df_fill.to_excel('ColorBrewer_all_schemes_RGBonly3_updated.XLS', sheet_name = 'Sheet1')
+
+# <codecell>
+
+df_fill.to_csv('ColorBrewer_all_schemes_RGBonly3_updated.csv')
+
+# <codecell>
+
+
