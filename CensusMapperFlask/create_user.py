0062bcfb22bf6e5bacf282da1f832e0c4098cdf6
Colin Gerber <colin.gerber@gmail.com>
2013-11-15 10:53:37 -0800

    updated

--- CensusMapperFlask/create_user.py
+++ CensusMapperFlask/create_user.py
@@ -30,6 +30,14 @@ class Map(db.Model):
     LatLongUpperRight = db.Column(db.Float)
     ZoomLevel = db.Column(db.Integer)
 
+    def __init__(self, MapName, ZoomLevel, LatLongUpperRight, LatLongLowerRight, LatLongUpperLeft, LatLongLowerLeft):
+        self.MapName = MapName
+        self.LatLongUpperRight = LatLongUpperRight
+        self.LatLongLowerRight = LatLongUpperRight
+        self.LatLongUpperLeft = LatLongUpperLeft
+        self.LatLongLowerLeft = LatLongLowerRight
+        self.ZoomLevel = ZoomLevel
+
 class DataLayers(db.Model):
     DataLayersID = db.Column(db.Integer, primary_key = True)
     MapID = db.Column(db.Integer, db.ForeignKey('Map.MapID'))
@@ -43,12 +51,27 @@ class DataLayers(db.Model):
     NumCategories = db.Column(db.Integer)
     Transparency = db.Column(db.Float)
 
+    def __init__(self, Year, DisplayOrder, DisplayGeography, DisplayType, Visible, ColorSchemeName, NumCategories, Transparency):
+        self.Year = Year
+        self.DisplayOrder = DisplayOrder
+        self.DisplayGeography = DisplayGeography
+        self.DisplayType = DisplayType
+        self.Visible = Visible
+        self.ColorSchemeName = ColorSchemeName
+        self.NumCategories = NumCategories
+        self.Transparency = Transparency
+
 class ValueBreaks(db.Model):
     ValueBreaksID = db.Column(db.Integer, primary_key = True)
     DataLayersID = db.Column(db.Integer, ForeignKey('DataLayers.DataLayersID'))
     CatgoryNumber = db.Column(db.Integer)
     MaxValue = db.Column(db.Float)
     MinValue = db.Column(db.Float)
+
+    def __init__(self, CatgoryNumber, MaxValue, MinValue):
+        self.CatgoryNumber = CatgoryNumber
+        self.MaxValue = MaxValue
+        self.MinValue = MinValue
 
 class ColorScheme(db.Model):
     ColorID = db.Column(db.Integer, primary_key = True)
@@ -59,17 +82,38 @@ class ColorScheme(db.Model):
     GreenValue = db.Column(db.Integer)
     BlueValue = db.Column(db.Integer)
 
+    def __init__(self, ColorSchemeName, NumCategories, CatgoryNumber, RedValue, GreenValue, BlueValue):
+        self.ColorSchemeName = ColorSchemeName
+        self.NumCategories = NumCategories
+        self.CatgoryNumber = CatgoryNumber
+        self.RedValue = RedValue
+        self.GreenValue = GreenValue
+        self.BlueValue = BlueValue
+
 class Measures(db.Model):
     MeasureID = db.Column(db.Integer, primary_key = True)
     Description = db.Column(db.String(200))
     NumeratorID = db.Column(db.Integer)
     DenominatorID = db.Column(db.Integer)
 
+    def __init__(self, Description, NumeratorID, DenominatorID):
+        self.Description = Description
+        self.NumeratorID = NumeratorID
+        self.DenominatorID = DenominatorID
+
 class Numerators(db.Model):
     NumeratorID = db.Column(db.Integer)
     FieldID = db.Column(db.Integer)
 
+    def __init__(self, NumeratorID, FieldID):
+        self.NumeratorID = NumeratorID
+        self.FieldID = FieldID
+
 class Denominators(db.Model):
     DenominatorID = db.Column(db.Integer)
     FieldID = db.Column(db.Integer)
+
+    def __init__(self, DenominatorID, FieldID):
+        self.DenominatorID = DenominatorID
+        self.FieldID = FieldID
         
