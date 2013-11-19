816ea108d1f1aee1e483e9b9269736b9219699f9
Jason Ost <jost@Jasons-MacBook-Air.local>
2013-11-19 15:47:00 -0800

    More updates to Flask

--- CensusMapperFlask/CensusMapperFlask/views.py
+++ CensusMapperFlask/CensusMapperFlask/views.py
@@ -4,10 +4,10 @@ from CensusMapperFlask import app
 import flask
 
 @app.route('/')
-def test():
+def home():
     return flask.render_template('home.html')
 
-@app.route('/create')
-def home():
+@app.route('/create_user')
+def create_user():
     return flask.render_template('create_account.html')
 
