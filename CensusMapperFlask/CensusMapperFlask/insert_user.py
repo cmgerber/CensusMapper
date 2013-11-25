f535f7e4066c0c57d4a5e110e869bce4091f6366
Jason Ost <jasonost@gmail.com>
2013-11-24 17:56:35 -0800

    Create user and login dialogs
    Still need to add flashes to indicate success or failure

--- CensusMapperFlask/CensusMapperFlask/insert_user.py
+++ CensusMapperFlask/CensusMapperFlask/insert_user.py
@@ -12,13 +12,15 @@ def create_account():
     #new user name input
     new_user_name = str(request.form['new_name'])
     new_email = str(request.form['new_email'])
-    new_password = md5(str(request.form['new_password'])).hexdigest()
+    new_password1 = md5(str(request.form['new_password1'])).hexdigest()
+    new_password2 = md5(str(request.form['new_password2'])).hexdigest()
     new_access = 'regular'
-    new_user = User(new_user_name, new_email, new_password, new_access)
-
-    #commits the new user to the db
-    db.session.add(new_user)
-    db.session.commit()
+    new_user = User(new_user_name, new_email, new_password1, new_access)
+    
+    if new_password1 == new_password2:
+        #commits the new user to the db
+        db.session.add(new_user)
+        db.session.commit()
 
     #flash('New account was successfully created. Please login.')
     return flask.render_template('logged_in.html', username = new_user_name)
